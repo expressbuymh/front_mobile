a55c0a17115213a0d9cd5ee319f47f3ce71b10eb
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-06-08 13:16:33 -0500

    Menu de opciones de categorias temporales, modificar mas adelante (#4)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, Dimensions, FlatList, } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 export const NavBar = () => {
@@ -13,42 +13,64 @@ export const NavBar = () => {
     setMenuExpanded(false);
   };
 
-  const menuItems = ['Item 1', 'Item 2', 'Item 3']; // Opciones del menú (pueden ser dinámicas)
+  const menuItems = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 8', 'Item 8', 'Item 8', 'Item 8', 'Item 8', 'Item 8', 'Item 8', 'Item 8', 'Item 8', 'Item 8',
+  ];
+
+  const screenWidth = Dimensions.get('window').width;
+  const menuWidth = screenWidth * 0.9;
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.item}>
+      <Text style={styles.itemText}>{item}</Text>
+      <View style={styles.itemSeparator} />
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={toggleMenu} style={styles.button}>
-        <Ionicons name={isMenuExpanded ? 'close' : 'menu'} size={24} color="white" />
-      </TouchableOpacity>
-      <Image source={require('../../assets/favicon.png')} />
-      {isMenuExpanded && (
+      <View style={styles.container1}>
+        <TouchableOpacity onPress={toggleMenu} style={styles.button}>
+          <Ionicons name={isMenuExpanded ? 'close' : 'menu'} size={24} color="white" />
+        </TouchableOpacity>
+        <Image source={require('../../assets/favicon.png')} />
+      </View>
+      <Modal visible={isMenuExpanded} animationType="slide" transparent={true}>
         <View style={styles.overlay}>
-          <TouchableOpacity onPress={closeMenu} style={styles.closeButton}>
-            <Ionicons name="close" size={24} color="black" />
-          </TouchableOpacity>
-          <View style={styles.menu}>
-            {menuItems.map((item, index) => (
-              <Text key={index}>{item}</Text>
-            ))}
+          <View style={styles.closeContainer}>
+            <TouchableOpacity onPress={closeMenu} style={styles.closeButton}>
+              <Ionicons name="close" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={[styles.menu, { width: menuWidth }]}>
+            <FlatList
+              data={menuItems}
+              renderItem={renderItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
           </View>
         </View>
-      )}
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
     width: '100%',
-    height: 100, // Cambio en la altura
+    height: 100,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'rgba(100, 10, 50, 1)',
+    zIndex: 1,
+  },
+  container1: {
+    width: '100%',
+    height: 100,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     backgroundColor: 'rgba(100, 10, 50, 1)',
-    zIndex: 1, // Asegura que el NavBar se muestre por encima de otros elementos
   },
   button: {
     backgroundColor: 'blue',
@@ -56,23 +78,46 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   overlay: {
-    position: 'absolute',
-    top: 100,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingTop: 50,
+    paddingLeft: 10,
   },
   menu: {
+    backgroundColor: 'white',
     padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    borderRadius: 5,
+    flex: 1,
   },
-  closeButton: {
+  closeContainer: {
     position: 'absolute',
     top: 10,
     right: 10,
   },
+  closeButton: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: 'white',
+  },
+  item: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  itemText: {
+    marginLeft: 10,
+    fontSize: 18,
+  },
+  itemSeparator: {
+    height: 1,
+    width: '100%',
+    marginTop: 5,
+    marginBottom: 5,
+    opacity: 0.5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'rgba(0, 0, 0, 0.2)',
+  },
 });
+
